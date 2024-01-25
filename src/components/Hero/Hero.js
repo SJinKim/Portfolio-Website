9e612c1e1d649e3f01944381cacf0c40e96e3392
SeungJin Kim <ksj_1207@hotmail.com>
2024-01-25 17:03:39 +0100

    slight adjustment

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -13,7 +13,15 @@ const Hero = (props) => (
         Welcome to <br />
         My Personal Webpage
       </SectionTitle>
-      <SectionText>I build interactive websites and applications</SectionText>
+      <SectionText>
+        Armed with a profound understanding of agile development methods, I am driven to create innovative
+        solutions while constantly striving for personal growth. My passion for clean code, coupled with
+        analytical thinking, empowers me to craft efficient and scalable solutions. I am dedicated to pushing
+        the boundaries of what is possible in the field of technology, and I embrace challenges as
+        opportunities for continuous improvement. My journey in the realm of computer science is not just a
+        pursuit of knowledge, but a commitment to shaping the future through cutting-edge, impactful
+        contributions.
+      </SectionText>
       <Button onClick={() => (window.location = 'https://github.com/SJinKim')}>To my Github</Button>
     </LeftSection>
   </Section>
